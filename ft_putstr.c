0ce73997d465b2d103634c79c93a2c8834b368d5
yon <valandil.yon@gmail.com>
2017-01-11 01:25:38 +0100

    ft_strlen done

--- ft_putstr.c
+++ ft_putstr.c
@@ -1,10 +1,10 @@
 #include "libft.h"
 
-void	ft_putstr(char *str)
+void	ft_putstr(char const *s)
 {
-	while(*str)
+	while(*s)
 	{
-		ft_putchar(*str);
-		str++;
+		ft_putchar(*s);
+		s++;
 	}
 }
---
+++ ft_strlen.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+int	ft_strlen(char const *s)
+{
+	int i;
+
+	i = 0;
+	while (*s)
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
