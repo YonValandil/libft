60eb267b3e56e78b93254150fee71846d5aef3e6
yon <valandil.yon@gmail.com>
2017-01-11 04:26:48 +0100

    strcmp bug a tester

--- libft.h
+++ libft.h
@@ -7,5 +7,6 @@ void	ft_putchar(char c);
 void	ft_putstr(char const *s);
 int	ft_strlen(char const *s);
 void	ft_swap(int *a, int *b);
+int	ft_strcmp(char const *s1, char const *s2);
 
 #endif
--- main.c
+++ main.c
@@ -1,24 +1,34 @@
 #include "libft.h"
 #include <stdio.h>
+#include <string.h>
 
 //int	main(int argc, char *argv[])
 int	main(void)
 {
-	int	l;
 	int	a;
 	int	b;
 
 	a = 5;
 	b = 9;
+
 	ft_putchar('Y');
-
 	ft_putstr("\nKonbanha\n");
-
-	l = ft_strlen("pikachu");
-	printf("ft_strlen = %d\n", l);
+	printf("ft_strlen = %d\n", ft_strlen("pikachu"));
 
 	printf("a = %d, b = %d\n", a, b);
 	ft_swap(&a, &b);
 	printf("a = %d, b = %d\n", a, b);
+
+	printf("strcmp = %d\n", strcmp("aaa", "aaa"));
+	printf("ft_strcmp = %d\n", ft_strcmp("aaa", "aaa"));
+
+	printf("strcmp = %d\n", strcmp("abcd", "abcf"));
+	printf("ft_strcmp = %d\n", ft_strcmp("abcd", "abcf"));
+
+	printf("strcmp = %d\n", strcmp("yoyo", "yoyoyoyo"));
+	printf("ft_strcmp = %d\n", ft_strcmp("yoyo", "yoyoyoyo"));
+
+	printf("strcmp = %d\n", strcmp("yoyoyoyo", "yoyo"));
+	printf("ft_strcmp = %d\n", ft_strcmp("yoyoyoyo", "yoyo"));
 	return (0);
 }
