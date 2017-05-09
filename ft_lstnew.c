0af21f6fa80cafec199910a56c4dfb432704818b
Jimmy JOURNE <jjourne@student.42.fr>
2017-05-09 02:39:47 +0200

    norm and update strjoin

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -1,12 +1,24 @@
-#include "libft.h" 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjourne <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/09 01:10:15 by jjourne           #+#    #+#             */
+/*   Updated: 2017/05/09 01:16:12 by jjourne          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
 
 t_list		*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list *new;
-	
-	if(!(new = ft_memalloc(sizeof(*new))))
+
+	if (!(new = ft_memalloc(sizeof(*new))))
 		return (NULL);
-	if(content == NULL)
+	if (content == NULL)
 	{
 		new->content = NULL;
 		new->content_size = 0;
--- ft_strjoin.c
+++ ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jjourne <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: jjourne <jjourne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/25 17:37:01 by jjourne           #+#    #+#             */
-/*   Updated: 2017/01/25 18:40:06 by jjourne          ###   ########.fr       */
+/*   Updated: 2017/05/09 02:35:12 by jjourne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,20 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		j;
 	char	*ns;
 
-	if (!s1 || !s2)
+	if (!s1 && !s2)
 		return (0);
-	i = 0;
-	j = 0;
+	if (!s1 || !s2)
+		return (ft_strdup((!s1) ? s2 : s1));
+	i = -1;
+	j = -1;
 	ns = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
 	if (ns == NULL)
 		return (NULL);
-	while (s1[i])
-	{
+	while (s1[++i])
 		ns[i] = s1[i];
-		++i;
-	}
-	while (s2[j])
+	while (s2[++j])
 	{
 		ns[i] = s2[j];
-		++j;
 		++i;
 	}
 	ns[i] = '\0';
